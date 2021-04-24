89229243a19884e327470c7cae8ada4c28f1856b
sammyoina <sammtoina@gmail.com>
2021-04-24 17:39:24 +0100

    csv files

--- modules/ignition.py
+++ modules/ignition.py
@@ -27,7 +27,7 @@ class Ignition(threading.Thread):
   
     def run(self):
         GPIO.output(self._actuator, GPIO.HIGH)
-        time.sleep(5)
+        time.sleep(20)
         while True:
             if self.stopped():
                 GPIO.output(self._actuator, GPIO.LOW)
